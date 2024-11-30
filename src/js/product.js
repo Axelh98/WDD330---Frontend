ac3386ac6604e6dbcaf413dbd4dac7ecad98176f
Axel Herrera <axelh260498@gmail.com>
2024-11-30 09:18:02 -0300

    fixing some errors

--- src/js/product.js
+++ src/js/product.js
@@ -8,27 +8,3 @@ const productId = getParam("product");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
-
-
-let cart = getLocalStorage("so-cart"); 
-if (!Array.isArray(cart)) {
-  cart = []; 
-}
-
-function addProductToCart(product) {
-  cart.push(product); // 
-  setLocalStorage("so-cart", cart); // Actualiza el almacenamiento local con el carrito actualizado
-}
-
-// Event handler para el botón de agregar al carrito
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id); 
-  addProductToCart(product); 
-  console.log(JSON.parse(localStorage.getItem("so-cart"))); 
-}
-
-
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
-
