83c6dbcf52f5294e57b050bf5a9c12b8a146d965
Axel Herrera <axelh260498@gmail.com>
2024-11-30 09:09:07 -0300

    fixing cart is not defined

--- src/js/product.js
+++ src/js/product.js
@@ -1,30 +1,32 @@
-import { getParam, loadHeaderFooter } from "./utils.mjs";
-import ExternalServices from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs";
+import { getParam, loadHeaderFooter, setLocalStorage, getLocalStorage } from "/js/utils.mjs";
+import ExternalServices from "/js/ProductData.mjs";
+import ProductDetails from "/js/ProductDetails.mjs";
 
 loadHeaderFooter();
-// <<<<<<< brunow2--individual
 const dataSource = new ExternalServices("tents");
 const productId = getParam("product");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
 
-    if (!Array.isArray(cart)) {
-      cart = [];
-    } 
 
-  cart.push(product);
-
-  setLocalStorage("so-cart", cart);
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  console.log(JSON.parse(localStorage.getItem("so-cart")));
-
-  addProductToCart(product);
+let cart = getLocalStorage("so-cart"); 
+if (!Array.isArray(cart)) {
+  cart = []; 
 }
+
+function addProductToCart(product) {
+  cart.push(product); // 
+  setLocalStorage("so-cart", cart); // Actualiza el almacenamiento local con el carrito actualizado
+}
+
+// Event handler para el botón de agregar al carrito
+async function addToCartHandler(e) {
+  const product = await dataSource.findProductById(e.target.dataset.id); 
+  addProductToCart(product); 
+  console.log(JSON.parse(localStorage.getItem("so-cart"))); 
+}
+
 
 document
   .getElementById("addToCart")
